b3b8ed597d89717baabb0230e86b5b8dab9d3ca3
Jordygit26 <pazcastillojordykevin@gmail.com>
2026-02-27 13:46:51 -0500

    Añadir la variables para almacenar la energia del hreoe y del enemigo
    Initialize hero and enemy energy variables.

--- nivel_2_funciones/batalla.js
+++ nivel_2_funciones/batalla.js
@@ -1,1 +1,7 @@
+// FASE 2 — BATALLA INTEGRADA
+// Reglas: Solo Variables, Funciones y Console.log
 
+
+//Declaramos  las variables que almacenarán la enrgia del heroe y el enemigo 
+let energiaHeroe = 100;
+let energiaEnemigo = 100;
