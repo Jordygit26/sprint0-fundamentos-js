d475a42847dfd389dc08e4f5e4ac835e99fc550e
Jordygit26 <pazcastillojordykevin@gmail.com>
2026-02-27 14:33:38 -0500

    Añadir la función ataqueJordy para gestionar ataques
    AQUI CREAMOS LA FUNCIÓN QUE NOS PERMIITRÁ BAJA LA ENERGIA DEL ENEMIGO DE JORDY , Y DEFINIR CUANTO DE EERGIA LO BAJARÁ

--- nivel_2_funciones/batalla.js
+++ nivel_2_funciones/batalla.js
@@ -2,5 +2,9 @@
 let energiajordy = 100;
 let energiaalessandro = 100;
 
-
+//FUNCIÓN ATAQUE DE JORDY HACIA ALESSANDRO
+function ataqueJordy() {
+    energiaAlessandro -= 20;
+    console.log("Jordy ataca. Energía de Alessandro: " + energiaAlessandro);
+}
 
