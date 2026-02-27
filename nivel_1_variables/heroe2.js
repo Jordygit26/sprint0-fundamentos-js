76c26fd27d8adcf802639d1675d769febc55dfe7
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-02-27 13:15:07 -0500

    Update heroe2.js
    ahora el heroe2 recibe daño critico, dejándolo en un estado vulnerable

--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -1,7 +1,7 @@
 // Fase 1: Simulador de Superhéroe
 const nombreHeroe = "Jordy"; 
 let poder = "Teletransportación de corto alcance"; 
-let nivelEnergia = 150; 
+let nivelEnergia = 100; 
 let ciudad = "Trujillo"; 
 let enemigo = "Sombra Voraz"; 
 
@@ -10,3 +10,8 @@ console.log(nombreHeroe + " vigila desde los techos de " + ciudad + ".");
 console.log("Gracias a su " + poder + ", puede moverse sin ser visto.");
 console.log("¡Atención! " + enemigo + " ha sido avistado en el centro.");
 console.log("Estado de energía: " + nivelEnergia + "%.");
+
+nivelEnergia = 1;
+
+console.log(nombreHeroe +  "ha recibido un daño critico de parte de" + enemigo );
+console.log("ahora su nivel de energia es de "+ nivelEnergia);
