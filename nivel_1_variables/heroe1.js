1b710d566c65ed0601dd13a051ac3f8a0d07895c
Jordygit26 <pazcastillojordykevin@gmail.com>
2026-02-27 12:24:42 -0500

    Log energy update after enemy attack
    modifico energia de heroe1 para simular batalla

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -6,3 +6,6 @@ const ciudad = "lima";
 const enemigo = "el cuy magico";
 console.log("Mi nombre es " + heroe1 + " mi poder es el de " + poder + ", mi energia es de " + nivelEnergia + ", mi ciudad natal es " + ciudad + " y mi enemigo es " + enemigo);
 
+// --- MI MODIFICACIÓN (Jordy) ---
+nivelEnergia = 30; 
+console.log("¡EMERGENCIA! El enemigo atacó. La nueva energía de " + heroe1 + " es: " + nivelEnergia);
