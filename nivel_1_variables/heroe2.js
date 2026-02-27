b1a170898af22677beda56395aff0c59d261abfc
Jordygit26 <pazcastillojordykevin@gmail.com>
2026-02-27 13:01:35 -0500

    Añadir simulación inicial de superhéroe en heroe2.js

--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -1,1 +1,12 @@
+// Fase 1: Simulador de Superhéroe
+let nombreHeroe = "Jordy"; 
+let poder = "Teletransportación de corto alcance"; 
+let nivelEnergia = 100; 
+let ciudad = "Trujillo"; 
+let enemigo = "Sombra Voraz"; 
 
+// Simulación de la historia inicial
+console.log(nombreHeroe + " vigila desde los techos de " + ciudad + ".");
+console.log("Gracias a su " + poder + ", puede moverse sin ser visto.");
+console.log("¡Atención! " + enemigo + " ha sido avistado en el centro.");
+console.log("Estado de energía: " + nivelEnergia + "%.");
