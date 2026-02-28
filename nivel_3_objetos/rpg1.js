9abdc0ba99665dd1e84e434e39570bbf0aeecc40
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-02-28 11:40:07 -0500

    Add player object and functions to modify attributes

---
+++ nivel_3_objetos/rpg1.js
@@ -0,0 +1,20 @@
+let jugador1 ={
+nombre:Alessandro,
+vida:100,
+fuerza:40,
+nivel:20,
+
+
+};
+function aumentarvidaAlessandro(){
+jugador1.vida +=30;
+console1.log("la vida de " + jugador1.nombre + "ha aumentado a " + jugador1.vida );
+};
+function aumentarfuerza(){
+jugador1.fuerza +=20;
+console.log("la fuerza de " + jugador1.nombre + " a aumentado a " + jugador1.fuerza);
+}
+function aumentarNivel(){
+jugador1.nivel +=1;
+console.log("el nivel de" + jugador1.nivel + " a subido, ahora es nivel " + jugador1.nivel );
+}
