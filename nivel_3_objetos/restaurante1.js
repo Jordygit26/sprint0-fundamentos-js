cc68a838c8169dfea5e33a5fd50bb75fa58074d5
Jordygit26 <pazcastillojordykevin@gmail.com>
2026-03-02 11:44:43 -0500

    Add ordenarComida function to reduce hunger

--- nivel_3_objetos/restaurante1.js
+++ nivel_3_objetos/restaurante1.js
@@ -4,3 +4,9 @@ let cliente = {
     dinero: 100, 
     hambre: 80   
 };
+
+// 2. FUNCIONES OBLIGATORIAS
+function ordenarComida(cliente) {
+    cliente.hambre -= 30; // Baja el nivel de hambre
+    console.log(cliente.nombre + " ordenó comida. Hambre actual: " + cliente.hambre);
+}
