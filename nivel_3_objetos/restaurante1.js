9d2f59c3974e4fd650b804d682cb44e841643df6
Jordygit26 <pazcastillojordykevin@gmail.com>
2026-03-02 11:45:03 -0500

    Add pagar function to handle payment processing

--- nivel_3_objetos/restaurante1.js
+++ nivel_3_objetos/restaurante1.js
@@ -10,3 +10,8 @@ function ordenarComida(cliente) {
     cliente.hambre -= 30; // Baja el nivel de hambre
     console.log(cliente.nombre + " ordenó comida. Hambre actual: " + cliente.hambre);
 }
+
+function pagar(cliente) {
+    cliente.dinero -= 40; // Resta dinero de la billetera
+    console.log(cliente.nombre + " pagó la cuenta. Saldo restante: " + cliente.dinero);
+}
