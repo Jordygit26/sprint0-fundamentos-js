21c5ff1bb5b17de7503a5005cbd195e57f3eb834
Alessandro <alessandrott2004@gmail.com>
2026-03-05 12:21:17 -0500

    se agregaron platos adicionales al menu

--- nivel_4_arrays/app.js
+++ nivel_4_arrays/app.js
@@ -2,7 +2,9 @@
 let menu = [
  { nombre: "Arroz con pollo", precio: 12, stock: 5 },
  { nombre: "Lomo saltado", precio: 18, stock: 3 },
- { nombre: "Sopa", precio: 8, stock: 10 }
+ { nombre: "Sopa", precio: 8, stock: 10 },
+ { nombre: "Causa rellena", precio: 20, stock:1},
+ { nombre: "Cuy chactado" , precio:30, stock:7}
 ];
 
 // 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
